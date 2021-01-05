5c50db8223d7dd58e6db1cb41872752c16cf904a
ikpb <ikpb@users.noreply.github.com>
2021-01-05 00:12:52 -0500

    added components and routes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
-import { MatSliderModule} from '@angular/material/slider'
+import { MatSliderModule } from '@angular/material/slider'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
-    imports:[MatSliderModule],
-    exports:[MatSliderModule]
+    imports:[MatSliderModule, MatButtonModule],
+    exports:[MatSliderModule, MatButtonModule]
 })
 export class MaterialModule{
 

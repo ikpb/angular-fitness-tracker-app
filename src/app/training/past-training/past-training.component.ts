f28251f874f9b93df94b2e914f34d5c4a4eeb96a
Your Name <you@example.com>
2021-01-11 16:12:10 -0500

    added pagination to app

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from 'src/app/model/exercise.model';
@@ -14,6 +15,7 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<Exercise>();
 
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private trainingService: TrainingService) { }
 
@@ -22,6 +24,7 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
   }
   ngAfterViewInit(){
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
   doFilter(filterValue:string){
     this.dataSource.filter = filterValue.trim().toLowerCase();

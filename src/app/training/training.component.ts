1749ac32e2cfa64383da0f76e090dcc57477dae4
Your Name <you@example.com>
2021-01-11 14:42:55 -0500

    using the training service to control state of the training portion of the app

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TrainingService } from '../service/training.service';
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.css']
 })
-export class TrainingComponent implements OnInit {
+export class TrainingComponent implements OnInit, OnDestroy{
 ongoingTraining = false;
-  constructor() { }
+exerciseSubscription: Subscription;
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit(): void {
+    this.exerciseSubscription = this.trainingService.exerciseChanged.subscribe(exerciseGiven =>{
+     this.ongoingTraining = (exerciseGiven) ? true: false;      
+    });
+  }
+  ngOnDestroy(){
+
   }
 
 }
